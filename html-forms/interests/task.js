04dd827e66e1ce41be7e2bc9cf9bb7f9166bd5b3
maria <mdymsakova@gmail.com>
2023-05-25 12:04:57 +0500

    second task fixed a bug

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -2,17 +2,11 @@ let interestCheck = document.querySelectorAll(".interest__check")
 
 interestCheck.forEach(function(check) {
     check.addEventListener("click", (event) => {
-        event.target.checked
-        let child = event.target.closest(".interest").querySelector(".interests_active")    
-        if (child) {
-            let childbox = child.querySelectorAll(".interest__check")
-            for ( let i = 0; i < childbox.length; i++) {
-                if (childbox[i].checked === true) {
-                    childbox[i].checked = false
-                } else if (childbox[i].checked === false) {
-                    childbox[i].checked = true
-                }
-            }   
-        }
+        let child = event.target.closest(".interest")  
+        let childbox = child.querySelectorAll(".interest__check")
+        for (let i = 0; i < childbox.length; i++) {
+            childbox[i].checked = event.target.checked
+            
+        }      
     })
 })
